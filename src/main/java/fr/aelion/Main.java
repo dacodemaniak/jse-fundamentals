f0d6e1dd28ee8b019ea1418eaecb5c8630fbe887
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-02-21 11:01:07 +0100

    Run course from Main

--- src/main/java/fr/aelion/Main.java
+++ src/main/java/fr/aelion/Main.java
@@ -1,9 +1,13 @@
 package fr.aelion;
 
+import fr.aelion.run.CourseRun;
+import fr.aelion.services.courses.DisplayCourse;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        CourseRun courseRun = new CourseRun();
+        courseRun.run();
     }
 
 }
---
+++ src/main/java/fr/aelion/run/CourseRun.java
@@ -0,0 +1,37 @@
+package fr.aelion.run;
+
+import fr.aelion.models.course.Document;
+import fr.aelion.models.course.Media;
+import fr.aelion.models.course.Slide;
+import fr.aelion.models.course.Video;
+import fr.aelion.repositories.course.Course;
+import fr.aelion.services.courses.DisplayCourse;
+
+public class CourseRun {
+    private Course course = new Course();
+
+    public CourseRun(){
+        this.course.setTitle("SQL");
+        this.makeCourse();
+    }
+
+    public void run() {
+        DisplayCourse displayCourse = new DisplayCourse();
+        displayCourse.setCourse(this.course);
+        displayCourse.display();
+    }
+    private void makeCourse() {
+        Media video = new Video();
+        video.setTitle("Create table");
+
+        Media slide = new Slide();
+        slide.setTitle("Alter table");
+
+        Media document = new Document();
+        document.setTitle("Drop table");
+
+        this.course.addMedia(video);
+        this.course.addMedia(slide);
+        this.course.addMedia(document);
+    }
+}
