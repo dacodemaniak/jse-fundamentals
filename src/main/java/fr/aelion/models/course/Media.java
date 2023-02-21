56d13b37b02975e7d22431d7dd123e651c3ddd6a
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-02-21 09:27:34 +0100

    Add Media abstract class

---
+++ src/main/java/fr/aelion/models/course/Media.java
@@ -0,0 +1,43 @@
+package fr.aelion.models.course;
+
+public abstract class Media {
+    protected String title;
+    protected String summary;
+    protected Float duration;
+    // @todo Create class Author and move type to
+    protected Author author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Float getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Float duration) {
+        this.duration = duration;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public abstract void play();
+}
