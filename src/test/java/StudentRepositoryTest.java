3db18ec178cafcdd33ff5fa33ec56a61e286d375
DaCodeManiaK <jla.webprojet@gmail.com>
2023-03-03 15:56:00 +0100

    Clean repository test

--- src/test/java/StudentRepositoryTest.java
+++ src/test/java/StudentRepositoryTest.java
@@ -73,43 +73,4 @@ public class StudentRepositoryTest {
                 () -> studentRepository.find(200)
         );
     }
-
-    /**
-    @Test
-    @DisplayName("Should return 'student' from model Student")
-    void shouldReturnTableName() {
-        assertEquals("student", studentRepository.getTableName());
-    }
-
-    @Test
-    @DisplayName("Should give 'student s' from Student class")
-    void shouldGiveAliasedTableName() {
-        assertEquals("student s", studentRepository.getAliasedTableName());
-    }
-
-    @Test
-    @DisplayName("Should get all attributes from model")
-    void shouldListAllAttributes() {
-        String[] expectedFields = {"email", "firstName", "id", "lastName", "login", "password", "phoneNumber"};
-        String[] classFields = studentRepository.getFields();
-        Arrays.sort(classFields);
-
-        assertTrue(Arrays.equals(expectedFields, classFields));
-    }
-
-    @Test
-    @DisplayName("Should return a comma separate list of column names")
-    void shouldReturnCommaSeparatedList() {
-        String expected = "s.email,s.first_name,s.id,s.last_name,s.login,s.password,s.phone_number";
-        assertEquals(expected, studentRepository.getCommaSeparatedColumns());
-    }
-
-    @Test
-    @DisplayName("Should return a full SELECT query")
-    void shouldReturnFullSelectQuery() {
-        String expected = "SELECT s.email,s.first_name,s.id,s.last_name,s.login,s.password,s.phone_number FROM student s;";
-
-        assertEquals(expected, studentRepository.getSelectQuery());
-    }
-    **/
 }
